cc4e630ded28e9afb43acdc396386f6e594c29a0
AntePost <is1213791@gmail.com>
2020-05-16 09:34:22 +0300

    add chapter 1 code

---
+++ chapter_1/myturtle.py
@@ -0,0 +1,59 @@
+from turtle import *
+
+shape("turtle")
+speed(0)
+
+
+def square(sidelength=100):
+    for i in range(4):
+        forward(sidelength)
+        right(90)
+
+
+def triangle(sidelength=100):
+    for i in range(3):
+        forward(sidelength)
+        right(120)
+
+
+def polygon(num_of_sides, sidelength=100):
+    for i in range(num_of_sides):
+        forward(sidelength)
+        right(360 / num_of_sides)
+
+
+def bigger_squares():
+    length = 5
+    for i in range(60):
+        square(length)
+        right(5)
+        length += 5
+
+def star(sidelength=100):
+    for i in range(5):
+        forward(sidelength)
+        right(144)
+        
+def star_spiral():
+    length = 5
+    for i in range(60):
+        star(length)
+        right(5)
+        length += 5
+
+# for i in range(60):
+#    square()
+#    right(5)
+
+# triangle()
+
+# polygon(6)
+
+#bigger_squares()
+
+#star()
+
+star_spiral()
+
+hideturtle()
+mainloop()
